253503eb2a00a2e8b5197e4a34fed9380c9c9232
Takahiro Yagushi <thnx4joiningus@gmail.com>
2018-11-09 19:41:01 +0900

    available to use serial port

---
+++ Project_Stand-up/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project_Stand-up/Assets/Scripts/TestSerialRead.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSerialRead : MonoBehaviour {
+
+    [SerializeField] SerialHandler serialHandler;
+
+	void Start () {
+        //信号を受信したときに、そのメッセージの処理を行う
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+	
+	void Update () {
+		
+	}
+
+    void OnDataReceived(string message)
+    {
+        Debug.Log("onReceived");
+
+        var data = message.Split(
+                new string[] { "\t" }, System.StringSplitOptions.None);
+        if (data.Length < 2) return;
+
+        try
+        {
+            Debug.Log(data[0].ToString());
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+
+}
